ad617d23d9df7c0104e056095ee10dcbbf4bdea7
tinyrack94 <winetree94@gmail.com>
2025-08-03 22:36:58 +0900

    chores: editor & linter 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ["en", "ko"],
+    defaultLocale: "en",
+  },
+
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,astro,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...eslintPluginAstro.configs.recommended,
+];
